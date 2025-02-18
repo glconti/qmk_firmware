e98d361add113b6767ee9aed669a39a8cdac9d70
Gianluigi Conti <gianluigi.conti@outlook.com>
2025-02-18 13:43:28 +0100

    Add my own firmware clone

---
+++ keyboards/sofle/keymaps/gigi/encoder.c
@@ -0,0 +1,34 @@
+#ifdef ENCODER_ENABLE
+
+#include "layers.h"
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) {
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+		} else if (index == 1) {
+			switch (get_highest_layer(layer_state)) {
+			case _RAISE:
+			case _LOWER:
+					if (clockwise) {
+						tap_code(KC_PGDN);
+					} else {
+						tap_code(KC_PGUP);
+					}
+				break;
+			default:
+					if (clockwise) {
+						tap_code(KC_WH_D);
+					} else {
+						tap_code(KC_WH_U);
+					}
+				break;
+		}
+    }
+    return true;
+}
+
+#endif
---
+++ keyboards/sofle/keymaps/gigi/layers.h
@@ -0,0 +1,11 @@
+#ifndef LAYERS_H
+#define LAYERS_H
+
+enum sofle_layers {
+    _COLEMAKDH = 0,
+    _LOWER,
+    _RAISE,
+    _NUMPAD
+};
+
+#endif // LAYERS_H
