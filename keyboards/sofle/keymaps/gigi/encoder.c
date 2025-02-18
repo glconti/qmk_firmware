ce51f69d19d4257475ae3cfb0018b5f6fde24c41
Gianluigi Conti <gianluigi.conti@outlook.com>
2025-02-18 23:06:37 +0100

    Improve keymap and oled

--- keyboards/sofle/keymaps/gigi/encoder.c
+++ keyboards/sofle/keymaps/gigi/encoder.c
@@ -1,6 +1,8 @@
+// #define ENCODER_ENABLE
+
 #ifdef ENCODER_ENABLE
 
-#include "layers.h"
+#    include "layers.h"
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
@@ -9,24 +11,24 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
         } else {
             tap_code(KC_VOLD);
         }
-		} else if (index == 1) {
-			switch (get_highest_layer(layer_state)) {
-			case _SYMBOL:
-			case _MOVE:
-					if (clockwise) {
-						tap_code(KC_PGDN);
-					} else {
-						tap_code(KC_PGUP);
-					}
-				break;
-			default:
-					if (clockwise) {
-						tap_code(KC_WH_D);
-					} else {
-						tap_code(KC_WH_U);
-					}
-				break;
-		}
+    } else if (index == 1) {
+        switch (get_highest_layer(layer_state)) {
+            case _SYMBOL:
+            case _MOVE:
+                if (clockwise) {
+                    tap_code(KC_PGDN);
+                } else {
+                    tap_code(KC_PGUP);
+                }
+                break;
+            default:
+                if (clockwise) {
+                    tap_code(MS_WHLD);
+                } else {
+                    tap_code(MS_WHLU);
+                }
+                break;
+        }
     }
     return true;
 }
