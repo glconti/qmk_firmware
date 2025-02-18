43e99ec13ddf525ab02934f558197363cf7cac80
Gianluigi Conti <gianluigi.conti@outlook.com>
2025-02-18 17:38:43 +0100

    Rgb matrix kinda works

--- keyboards/sofle/keymaps/gigi/config.h
+++ keyboards/sofle/keymaps/gigi/config.h
@@ -21,13 +21,7 @@
 #pragma once
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 4
-
-// Vial Support
-#define VIAL_KEYBOARD_UID { 0x05, 0xCD, 0x9F, 0x8A, 0xF4, 0xDF, 0xDE, 0xB2 }
-
-// The four corners
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 5, 3, 8 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 0, 0, 0 }
+#define GRAVE_ESC_CTRL_OVERRIDE
 
 #undef SWAP_HANDS_ENABLE
 
@@ -59,8 +53,9 @@
 
 
 #ifdef RGB_MATRIX_ENABLE
-#define RGBLIGHT_LED_COUNT 35    // Number of LEDs
-#define RGBLIGHT_LED_COUNT 35    // Number of LEDs
+#undef RGBLIGHT_LED_COUNT
+#undef RGB_MATRIX_LED_COUNT
+#define RGBLIGHT_LED_COUNT 60    // Number of LEDs
 #define RGB_MATRIX_LED_COUNT RGBLIGHT_LED_COUNT
 #endif
 
@@ -96,10 +91,8 @@
 #   define RGB_MATRIX_SLEEP // turn off effects when suspended
 #   define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 // #   define RGB_MATRIX_LED_PROCESS_LIMIT (RGB_MATRIX_LED_COUNT + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
-// #   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
+#   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
 #    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
-
-#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_GRADIENT_LEFT_RIGHT
 
 #    define RGB_MATRIX_HUE_STEP 8
 #    define RGB_MATRIX_SAT_STEP 8
@@ -108,33 +101,36 @@
 
 /* Disable the animations you don't want/need.  You will need to disable a good number of these    *
  * because they take up a lot of space.  Disable until you can successfully compile your firmware. */
- // #   undef ENABLE_RGB_MATRIX_ALPHAS_MODS
- // #   undef ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
- // #   undef ENABLE_RGB_MATRIX_BREATHING
- // #   undef ENABLE_RGB_MATRIX_CYCLE_ALL
- // #   undef ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
- // #   undef ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
- // #   undef ENABLE_RGB_MATRIX_CYCLE_OUT_IN
- // #   undef ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
- // #   undef ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
- // #   undef ENABLE_RGB_MATRIX_DUAL_BEACON
- // #   undef ENABLE_RGB_MATRIX_RAINBOW_BEACON
- // #   undef ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS
- // #   undef ENABLE_RGB_MATRIX_RAINDROPS
- // #   undef ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
- // #   undef ENABLE_RGB_MATRIX_TYPING_HEATMAP
- // #   undef ENABLE_RGB_MATRIX_DIGITAL_RAIN
- // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE
- // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
- // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
- // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
- // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
- // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
- // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
- // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
- // #   undef ENABLE_RGB_MATRIX_SPLASH
- // #   undef ENABLE_RGB_MATRIX_MULTISPLASH
- // #   undef ENABLE_RGB_MATRIX_SOLID_SPLASH
- // #   undef ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
+ #   define ENABLE_RGB_MATRIX_ALPHAS_MODS
+ #   define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+ #   define ENABLE_RGB_MATRIX_BREATHING
+ #   define ENABLE_RGB_MATRIX_CYCLE_ALL
+ #   define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+ #   define ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
+ #   define ENABLE_RGB_MATRIX_CYCLE_OUT_IN
+ #   define ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+ #   define ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+ #   define ENABLE_RGB_MATRIX_DUAL_BEACON
+ #   define ENABLE_RGB_MATRIX_RAINBOW_BEACON
+ #   define ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+ #   define ENABLE_RGB_MATRIX_RAINDROPS
+ #   define ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+ #   define ENABLE_RGB_MATRIX_TYPING_HEATMAP
+ #   define ENABLE_RGB_MATRIX_DIGITAL_RAIN
+ #   define ENABLE_RGB_MATRIX_SOLID_REACTIVE
+ #   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+ #   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+ #   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+ #   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+ #   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+ #   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+ #   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+ #   define ENABLE_RGB_MATRIX_SPLASH
+ #   define ENABLE_RGB_MATRIX_MULTISPLASH
+ #   define ENABLE_RGB_MATRIX_SOLID_SPLASH
+ #   define ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
+
+ #   define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_DIGITAL_RAIN
+
 #endif
 
