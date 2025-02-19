32673230b1344df9e9aff43a17c70e64b78d7729
Gianluigi Conti <gianluigi.conti@outlook.com>
2025-02-19 23:58:44 +0100

    Move shift and ctrl

--- keyboards/sofle/keymaps/gigi/keymap.c
+++ keyboards/sofle/keymaps/gigi/keymap.c
@@ -49,6 +49,7 @@ tap_dance_action_t tap_dance_actions[] = {
 #define KC_TAB_NUM LT(_NUMPAD, KC_TAB)
 #define KC_LSHIFT_CAPS TD(TD_LSHIFT_CAPS)
 #define KC_RSHIFT_CAPS TD(TD_RSHIFT_CAPS)
+#define KC_MUTE_PLAY TD(TD_MUTE_PLAY)
 #define KC_ESC_GRV TD(TD_ESC_GRV)
 #define KC_SYMBO MO(_SYMBOL)
 #define KC_MOVE MO(_MOVE)
@@ -77,8 +78,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS,
             KC_DEL,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_BSPC,
           KC_TAB_NUM, KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                KC_M,    KC_N,    KC_E,    KC_I,    KC_O,  KC_QUOT,
-            KC_LGUI,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V, KC_MUTE,       KC_HOME,  KC_K,    KC_H,    KC_COMM, KC_DOT, KC_SLSH,  KC_INS,
-                KC_LALT ,  KC_LCTL, KC_LSHIFT_CAPS , KC_SYMBO , KC_SPC,       KC_ENT,  KC_MOVE, KC_RSHIFT_CAPS, KC_RCTL, KC_RALT
+                 KC_LSHIFT_CAPS, KC_Z, KC_X, KC_C, KC_D, KC_V, KC_MUTE_PLAY,  KC_HOME,  KC_K,    KC_H,    KC_COMM, KC_DOT, KC_SLSH,  KC_RSHIFT_CAPS,
+                    KC_LCTL, KC_LGUI, KC_LALT, KC_SYMBO , KC_SPC,       KC_ENT,  KC_MOVE, KC_RALT, KC_INS, KC_RCTL
 ),
 /* SYMBOL
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -95,11 +96,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_SYMBOL] = LAYOUT(
-   XXXXXXX,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_PLUS,
-   XXXXXXX,   KC_F11, KC_F12  , XXXXXXX,XXXXXXX,XXXXXXX,                       KC_MPRV, KC_MNXT, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX,
-   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, KC_PIPE,
-   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,       _______, _______, _______, XXXXXXX, KC_EQL, KC_BSLS, _______,
-                     _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
+   _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
+   _______,   XXXXXXX, XXXXXXX, XXXXXXX,XXXXXXX,XXXXXXX,                       KC_MPRV, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, KC_F12,
+   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       KC_MPLY, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, KC_PIPE,
+   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,     _______, KC_MNXT, _______, XXXXXXX, KC_EQL, KC_BSLS, _______,
+                     _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______
 ),
 /* MOVE
  * ,----------------------------------------.                     ,-----------------------------------------.
@@ -116,10 +117,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_MOVE] = LAYOUT(
-  XXXXXXX, XXXXXXX , XXXXXXX , UG_TOGG, UG_PREV , UG_NEXT,                      XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX,  XXXXXXX,  XXXXXXX,  LM_TOGG, LM_PREV, LM_NEXT,                     KC_PGUP, XXXXXXX,   KC_UP,  XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  LM_BRID, LM_BRIU,                       KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT,  XXXXXXX, XXXXXXX,
-  XXXXXXX,KC_UNDO, KC_CUT, KC_COPY, KC_PASTE, XXXXXXX,  _______,       _______,  KC_HOME,  KC_END, XXXXXXX, XXXXXXX,   XXXXXXX, _______,
+  _______, XXXXXXX , XXXXXXX , UG_TOGG, UG_PREV , UG_NEXT,                      XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, _______,
+  _______,  XXXXXXX,  XXXXXXX,  LM_TOGG, LM_PREV, LM_NEXT,                     KC_PGUP, XXXXXXX,   KC_UP,  XXXXXXX, XXXXXXX, _______,
+  _______, XXXXXXX,  XXXXXXX,  XXXXXXX,  LM_BRID, LM_BRIU,                       KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT,  XXXXXXX, _______,
+  _______,KC_UNDO, KC_CUT, KC_COPY, KC_PASTE, XXXXXXX,  _______,       _______,  KC_HOME,  KC_END, XXXXXXX, XXXXXXX,   XXXXXXX, _______,
                     _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
 ),
 
